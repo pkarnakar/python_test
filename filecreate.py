d903c150c4cad72ce707e8349aeebfb81fd512d9
pkarnakar <42792400+pkarnakar@users.noreply.github.com>
2019-10-05 08:58:33 -0400

    Add files via upload

---
+++ filecreate.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 11 10:22:44 2018
+
+@author: karan
+"""
+
+file=open("C:\\Users\\karan\\Documents\\Python Scripts\\text.txt",'w')
+
---
+++ files.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 10 11:23:32 2018
+
+@author: karan
+"""
+
+filelist=list(range(2000,2020,2))
+#print(filelist)
+filelist[1:4]
